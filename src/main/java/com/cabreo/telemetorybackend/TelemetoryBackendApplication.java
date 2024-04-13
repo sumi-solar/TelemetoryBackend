8052aa17aab083e56fa74afb7ef8fa8e7b6b57e5
sumi-solar <nnishioka1024@gmail.com>
2024-04-13 23:49:03 +0900

    Initial commit

---
+++ src/main/java/com/cabreo/telemetorybackend/TelemetoryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cabreo.telemetorybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemetoryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelemetoryBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cabreo/telemetorybackend/TelemetoryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cabreo.telemetorybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelemetoryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
